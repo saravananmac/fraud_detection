597adbde8c7686309160e7402fc0be5462812ba5
saravananmac <saravananmac@gmail.com>
2023-03-26 18:00:53 +0530

    all file updated

--- data_processing.py
+++ data_processing.py
@@ -7,5 +7,11 @@ def add(a,b):
     c=a+b
     print ("sum of a and b is:", c)
 
+def sub(a,b):
+    d=b-a
+    print("sub of a and b is:", d)
+
 add(a,b)
-print ("sum done successfully")
+sub(a,b)
+print ("sum & sub done successfully")
+
