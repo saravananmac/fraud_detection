cd970bfb03ece9d7c324ca35dbdbd223e6d0ae41
saravananmac <saravananmac@gmail.com>
2023-03-26 18:26:54 +0530

    updated model detaisl in the data_processing1.py

--- data_processing1.py
+++ data_processing1.py
@@ -1,3 +1,12 @@
 print("this is sample data processing1 file")
 
 print("adding all ML model related code here")
+
+def model1():
+    print("model 1 algo")
+
+def model2():
+    print("model 2 algo")
+
+model1()
+model2()
