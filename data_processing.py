a3c0e68a4ff927af8205417c84dfdd11ba13a88b
saravananmac <saravananmac@gmail.com>
2023-03-26 18:05:06 +0530

    code for mul updated successfully

--- data_processing.py
+++ data_processing.py
@@ -11,7 +11,12 @@ def sub(a,b):
     d=b-a
     print("sub of a and b is:", d)
 
+def mul(a,b):
+    c=a*b
+    print("mul of a and b is:", c)
+
 add(a,b)
 sub(a,b)
-print ("sum & sub done successfully")
+mul(a,b)
+print ("All calculations done successfully")
 
